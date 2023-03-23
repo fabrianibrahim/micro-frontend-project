5fa7180c4ea271582ce8072b240f0532c32d5bbf
Fabrian Ibrahim <fabrian.ibrahim@gmail.com>
2023-03-23 21:33:23 +1100

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, 'bootstrap', 'dist')));
+
+app.use('/mfe/welcome', express.static(path.join(__dirname, 'welcome', 'dist')));
+
+app.use('/mfe/music', express.static(path.join(__dirname, 'music', 'build')));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
